c10dd47b2d470c8cb2a48d8be37718ed7ba6e30e
Waldemar2023 <waldemar.fredrich@gmail.com>
2023-05-22 15:23:47 +0200

    header changes added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home'
 import Contact from './components/Contact'
 import Story from './components/Story'
 
+
 function App() {
 	const router = createBrowserRouter(
 		createRoutesFromElements(
