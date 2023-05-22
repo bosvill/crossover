6697f82035050feb6a534e551d47abdd0afaa6fd
bosvill <sv.boecker@gmail.com>
2023-05-22 15:34:51 +0200

    story

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Card.module.css'
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <h3>Lorem ipsum</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ipsa consequatur et aspernatur iure reiciendis fugit. Praesentium cum alias eum ratione asperiores soluta sed quos quisquam.</p>
+            
+        </div>
+    );
+}
+
+export default Card;
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from 'react'
+import styles from './Story.module.css'
+import Card from './Card'
 
 const Story = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			<section className={styles.section1}>
+				<div className={styles.content1}>
+					<h1 className={styles.h1}>Our story</h1>
+					<p className={styles.p}>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi atque officiis
+						perferendis aliquid nihil odit sit, fugiat placeat excepturi incidunt consectetur eaque.
+						Reiciendis quia maiores eligendi sapiente, quidem voluptatibus. Ipsa aspernatur eum
+						pariatur. Id ea, illo quidem maiores quam enim.
+					</p>
+				</div>
+			</section>
+			<section className={styles.section2}>
+				<div className={styles.content2}>
+					<h2 className={styles.h2}>Explore</h2>
+					<h3 className={styles.h3}>The world</h3>
+				</div>
+			</section>
+			<section className={styles.section3}>
+				<Card />
+				<Card />
+				<Card />
+			</section>
+		</div>
+	)
 }
 
-export default Story;
+export default Story
